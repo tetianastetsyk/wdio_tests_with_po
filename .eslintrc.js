9e3978dc04073cee7e9c3659fc93d0f456dba56e
Artem_Ovcharenko <ov.artem90@gmail.com>
2023-06-22 11:43:31 +0300

    eslint + small fixes

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+        mocha: true,
+    },
+    globals: {
+        browser: true,
+        driver: true,
+        $: true,
+        $$: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:import/recommended',
+        'plugin:wdio/recommended',
+    ],
+    plugins: [
+        'wdio',
+        'import',
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'class-methods-use-this': 'off',
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+            },
+        ],
+        'max-len': ['error', {
+            code: 120,
+            ignoreComments: true,
+            ignoreTrailingComments: true,
+            ignoreUrls: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+            ignoreRegExpLiterals: true,
+        }],
+    },
+};
